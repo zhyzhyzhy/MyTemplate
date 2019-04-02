a7eea8be5a191abcee1b85181a684be826959cc1
zhyzhyzhy <1012146386@qq.com>
2019-04-02 15:45:55 +0800

    init

--- spring-boot-actuator/src/main/java/cc/lovezhy/template/actuator/HelloController.java
+++ spring-boot-actuator/src/main/java/cc/lovezhy/template/actuator/HelloController.java
@@ -1,4 +1,16 @@
 package cc.lovezhy.template.actuator;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public Map<String, String> index() {
+        return Collections.singletonMap("Hello", "world");
+    }
 }
